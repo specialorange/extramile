8c0ac866ec78a9bb865024fe365573d5e00cd6be
special <github@specialorange.org>
2013-11-15 11:38:59 -0500

    initial starting of the website

---
+++ about.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About - Extra Mile Vending</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <?php include 'shared/nav.html'; ?>
+
+    <div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-12">
+          <h1 class="page-header">About <small>our story</small></h1>
+          <ol class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li class="active">About</li>
+          </ol>
+        </div>
+
+      </div>
+
+      <div class="row">
+
+        <div class="col-md-6">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+        </div>
+        <div class="col-md-6">
+          <h2>Going the Extra Mile</h2>
+          <p>Extra Mile Vending opened its doors in January of 2004 with 10 soda machines.  Through our customer referrals we have grown to over 800 machines including soda, snack, arcades, bar games, bulk vending machines and more.  We set up & service machines within a 50 mile radius of Charlotte and have 3 locations in Charlotte, Concord and Statesville to best serve our customers.  Look for upcoming news on our 4th and newest location opening soon in Shelby which will serve as our arcade game headquarters and also the home for 2 new vending routes.</p>
+
+          <p>In 2010, we opened a new used equipment division to provide top quality refurbished and affordable equipment to smaller vendors and independent business owners wishing to own and operate their own vending equipment.  See our used equipment page for our current inventory.</p>
+        </div>
+
+      </div>
+
+      <!-- Team Member Profiles -->
+
+      <div class="row">
+
+        <div class="col-lg-12">
+          <h2 class="page-header">Our Team</h2>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Jeff Dahlgard <small>President</small></h3>
+            <h4><small>704-564-6004</small></h4>
+            <h4><small><a href="mailto:jeff@extramilevending.com">jeff@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Otis Forsythe Jr. <small>Key Accounts Manger</small></h3>
+            <h4><small>704-858-4516</small></h4>
+            <h4><small><a href="mailto:otis@extramilevending.com">otis@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Daniel Smith <small>Warehouse Manger</small></h3>
+            <h4><small>&nbsp</small></h4>
+            <h4><small><a href="mailto:daniel@extramilevending.com">daniel@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Denny Blevins <small>New Account Sales Manager</small></h3>
+            <h4><small>828-612-5161</small></h4>
+            <h4><small><a href="mailto:denny@extramilevending.com">denny@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Josh Nanney <small>South Charlotte Sales Manger</small></h3>
+            <h4><small>704-737-1045</small></h4>
+            <h4><small><a href="mailto:josh@extramilevending.com">josh@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="col-sm-4">
+        	<img class="img-responsive" src="http://placehold.it/750x450">
+            <h3>Melissa Outz <small>Invoices/Accounting</small></h3>
+            <h4><small>&nbsp</small></h4>
+            <h4><small><a href="mailto:melissa@extramilevending.com">melissa@extramilevending.com</a></small></h4>
+            <p>What does this team member do? Keep it short! This is also a great spot for social links!</p>
+            <ul class="list-unstyled list-inline list-social-icons">
+              <li class="tooltip-social facebook-link"><a href="#facebook-profile" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+              <li class="tooltip-social linkedin-link"><a href="#linkedin-profile" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
+              <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
+              <li class="tooltip-social google-plus-link"><a href="#google-plus-profile" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
+            </ul>
+        </div>
+
+      </div>
+
+      <!-- Our Customers -->
+
+      <div class="row">
+
+        <div class="col-lg-12">
+          <h2 class="page-header">Our Customers</h2>
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+        <div class="col-md-2 col-sm-4 col-xs-6">
+        	<img class="img-responsive img-customer" src="http://placehold.it/500x300">
+        </div>
+
+      </div>
+
+    </div><!-- /.container -->
+
+    <div class="container">
+
+    <hr>
+    <?php include 'shared/footer.html'; ?>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modern-business.js"></script>
+  </body>
+</html>
---
+++ services.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Services - Modern Business - Start Bootstrap Template</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <!-- Add custom CSS here -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+  <?php include 'shared/nav.html'; ?>
+
+    <!-- Page Content -->
+
+    <div class="container">
+      
+      <div class="row">
+      
+        <div class="col-lg-12">
+          <h1 class="page-header">Services <small>What We Do</small></h1>
+          <ol class="breadcrumb">
+            <li><a href="index.php">Home</a></li>
+            <li class="active">Services</li>
+          </ol>
+        </div>
+
+      </div><!-- /.row -->
+
+      <div class="row">
+      
+        <div class="col-lg-12">
+          <img class="img-responsive" src="http://placehold.it/1200x300">
+        </div>
+
+      </div><!-- /.row -->
+
+      <!-- Service Paragraphs -->
+
+      <div class="row">
+
+        <div class="col-md-8">
+          <h2 class="page-header">Our Premium Services</h2>
+            <p>Let Extra Mile Vending service your business's vending needs.  We use only top of the line new or refurbished vending equipment and have the friendliest staff in the industry.  To our vending customers, we offer soda & snack machines as well as sandwich and coffee machines for businesses with a large number of employees and short break times or long shifts.  For our arcade and restaurant customers, we offer a wide range of arcade games, bar games, candy and toy cranes and bulk vending machines.</p>
+
+            <p>Although we cannot promise a machine will never breakdown, we can assure you that all of our route drivers are highly trained in preventative maintenance and we have 2 full time techs on hand to handle any issues that arise quickly, in most cases within 24 hours.</p>
+
+            <p>Let us show you the "Extra Mile" difference!</p>
+        </div>
+
+        <div class="col-md-4">
+          <h2 class="page-header">Something More</h2>
+            <p>Etharums ser quidem rerum facilis dolores nemis omnis fugats vitaes nemo minima rerums unsers sadips amets.</p>
+            <a class="btn btn-primary" href="#">Click Me!</a>
+        </div>
+
+      </div><!-- /.row -->
+
+      <!-- Service Tabs -->
+
+      <div class="row">
+
+        <div class="col-lg-12">
+          <h2 class="page-header">Tabbed Services</h2>
+          <ul id="myTab" class="nav nav-tabs">
+            <li class="active"><a href="#service-one" data-toggle="tab">Service One</a></li>
+            <li><a href="#service-two" data-toggle="tab">Service Two</a></li>
+            <li><a href="#service-three" data-toggle="tab">Service Three</a></li>
+            <li><a href="#service-four" data-toggle="tab">Service Four</a></li>
+            <li><a href="#service-five" data-toggle="tab">Service Five</a></li>
+          </ul>
+          <div id="myTabContent" class="tab-content">
+            <div class="tab-pane fade in active" id="service-one">
+              <i class="fa fa-gear pull-left fa-4x"></i>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam placerat nunc ut tellus tristique, non posuere neque iaculis. Fusce aliquet dui ut felis rhoncus, vitae molestie mauris auctor. Donec pellentesque feugiat leo a adipiscing. Pellentesque quis tristique eros, sed rutrum mauris.</p>
+              <p>Nam fringilla quis enim in eleifend. Suspendisse sed lectus mauris. Nam commodo, arcu et posuere placerat, tellus tortor dignissim eros, sit amet eleifend urna lorem sit amet nulla. Praesent sem nibh, vulputate nec congue eu, dapibus vitae augue. Suspendisse cursus urna sit amet metus porttitor, in pharetra quam feugiat. Etiam tempus euismod nulla eget pellentesque.</p>
+            </div>
+            <div class="tab-pane fade" id="service-two">
+              <i class="fa fa-gears pull-left fa-4x"></i>
+              <p>Nam fringilla quis enim in eleifend. Suspendisse sed lectus mauris. Nam commodo, arcu et posuere placerat, tellus tortor dignissim eros, sit amet eleifend urna lorem sit amet nulla. Praesent sem nibh, vulputate nec congue eu, dapibus vitae augue. Suspendisse cursus urna sit amet metus porttitor, in pharetra quam feugiat. Etiam tempus euismod nulla eget pellentesque.</p>
+              <p>Vestibulum laoreet molestie urna ac vehicula. Phasellus laoreet semper ipsum ac gravida. Sed in varius tortor. Nullam blandit in neque quis aliquet. Fusce volutpat pellentesque sem non convallis. Suspendisse sit amet magna pulvinar, gravida mauris eu, tincidunt massa. Nam lectus mi, viverra non quam nec, mollis malesuada dolor. Vivamus hendrerit nunc interdum turpis egestas, a lobortis odio consequat. Fusce posuere purus quis ligula faucibus lacinia. Curabitur sit amet congue dolor. Duis dapibus hendrerit nunc et gravida. Phasellus mollis, lectus quis ornare aliquam, arcu orci posuere lectus, vehicula bibendum sem ante quis lacus.</p>
+            </div>
+            <div class="tab-pane fade" id="service-three">
+              <i class="fa fa-magic pull-left fa-4x"></i>
+              <p>Vestibulum laoreet molestie urna ac vehicula. Phasellus laoreet semper ipsum ac gravida. Sed in varius tortor. Nullam blandit in neque quis aliquet. Fusce volutpat pellentesque sem non convallis. Suspendisse sit amet magna pulvinar, gravida mauris eu, tincidunt massa. Nam lectus mi, viverra non quam nec, mollis malesuada dolor. Vivamus hendrerit nunc interdum turpis egestas, a lobortis odio consequat. Fusce posuere purus quis ligula faucibus lacinia. Curabitur sit amet congue dolor. Duis dapibus hendrerit nunc et gravida. Phasellus mollis, lectus quis ornare aliquam, arcu orci posuere lectus, vehicula bibendum sem ante quis lacus.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam placerat nunc ut tellus tristique, non posuere neque iaculis. Fusce aliquet dui ut felis rhoncus, vitae molestie mauris auctor. Donec pellentesque feugiat leo a adipiscing. Pellentesque quis tristique eros, sed rutrum mauris.</p>
+            </div>
+            <div class="tab-pane fade" id="service-four">
+              <i class="fa fa-flask pull-left fa-4x"></i>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam placerat nunc ut tellus tristique, non posuere neque iaculis. Fusce aliquet dui ut felis rhoncus, vitae molestie mauris auctor. Donec pellentesque feugiat leo a adipiscing. Pellentesque quis tristique eros, sed rutrum mauris.</p>
+              <p>Nam fringilla quis enim in eleifend. Suspendisse sed lectus mauris. Nam commodo, arcu et posuere placerat, tellus tortor dignissim eros, sit amet eleifend urna lorem sit amet nulla. Praesent sem nibh, vulputate nec congue eu, dapibus vitae augue. Suspendisse cursus urna sit amet metus porttitor, in pharetra quam feugiat. Etiam tempus euismod nulla eget pellentesque.</p>
+            </div>
+            <div class="tab-pane fade" id="service-five">
+              <i class="fa fa-flag pull-left fa-4x"></i>
+              <p>Nam fringilla quis enim in eleifend. Suspendisse sed lectus mauris. Nam commodo, arcu et posuere placerat, tellus tortor dignissim eros, sit amet eleifend urna lorem sit amet nulla. Praesent sem nibh, vulputate nec congue eu, dapibus vitae augue. Suspendisse cursus urna sit amet metus porttitor, in pharetra quam feugiat. Etiam tempus euismod nulla eget pellentesque.</p>
+              <p>Vestibulum laoreet molestie urna ac vehicula. Phasellus laoreet semper ipsum ac gravida. Sed in varius tortor. Nullam blandit in neque quis aliquet. Fusce volutpat pellentesque sem non convallis. Suspendisse sit amet magna pulvinar, gravida mauris eu, tincidunt massa. Nam lectus mi, viverra non quam nec, mollis malesuada dolor. Vivamus hendrerit nunc interdum turpis egestas, a lobortis odio consequat. Fusce posuere purus quis ligula faucibus lacinia. Curabitur sit amet congue dolor. Duis dapibus hendrerit nunc et gravida. Phasellus mollis, lectus quis ornare aliquam, arcu orci posuere lectus, vehicula bibendum sem ante quis lacus.</p>
+            </div>
+          </div>
+        </div>
+
+      </div><!-- /.row -->
+
+      <!-- Service Images -->
+
+      <div class="row">
+
+        <div class="col-lg-12">
+          <h2 class="page-header">Service Images</h2>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+          <h3>Service One</h3>
+          <p>Service one description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst.</p>
+          <a class="btn btn-link btn-sm pull-right">More <i class="fa fa-angle-right"></i></a>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+          <h3>Service Two</h3>
+          <p>Service two description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst.</p>
+          <a class="btn btn-link btn-sm pull-right">More <i class="fa fa-angle-right"></i></a>
+        </div>
+
+        <div class="col-sm-4">
+          <img class="img-responsive" src="http://placehold.it/750x450">
+          <h3>Service Three</h3>
+          <p>Service three description. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc placerat diam quis nisl vestibulum dignissim. In hac habitasse platea dictumst.</p>
+          <a class="btn btn-link btn-sm pull-right">More <i class="fa fa-angle-right"></i></a>
+        </div>
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <div class="container">
+
+    <hr>
+    <?php include 'shared/footer.html'; ?>
+
+    </div><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/modern-business.js"></script>
+  </body>
+</html>
